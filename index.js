e0224d53265c96d9e00efae4ee3efb0a6499aea2
Saad Rhoulam <saad@rhoulam.com>
2016-03-16 16:32:58 -0400

    Overhaul tests to run multiple tests in sequence.

--- index.js
+++ index.js
@@ -7,18 +7,65 @@ document.addEventListener("DOMContentLoaded", function() {
             event.stopPropagation();
 
             //  Test cases
-            runTest({
-                'collection' : (function() {
-                    var collection = [];
-                    for(var count = 0; count < 10000; count++) {
-                        collection.push(count > 1023 ? 1023 : count);
+            var testCases = [function(callback) {
+                runTest({
+                    'collection' : (function() {
+                        var collection = [];
+                        for(var count = 0; count < 10000; count++) {
+                            collection.push(count > 1023 ? 1023 : count);
+                        }
+                        return collection;
+                    })(),
+                    mappingFn : function(element) {
+                        return Math.pow(2, element);
+                    },
+                    ymOptions : {
+                        batchSize : 1000,
+                        delayPeriod : 1000
                     }
-                    return collection;
-                })(),
-                mappingFn : function(element) {
-                    return Math.pow(2, element);
-                }
-            });
+                }, callback);
+            },
+            function(callback) {
+                runTest({
+                    'collection' : (function() {
+                        var collection = [];
+                        for(var count = 0; count < 10000; count++) {
+                            collection.push(count > 1023 ? 1023 : count);
+                        }
+                        return collection;
+                    })(),
+                    mappingFn : function(element) {
+                        return Math.pow(2, element);
+                    },
+                    ymOptions : {
+                        batchSize : 100
+                    }
+                }, callback);
+            },
+            function(callback) {
+                runTest({
+                    'collection' : (function() {
+                        var collection = [];
+                        for(var count = 0; count < 10000; count++) {
+                            collection.push(count > 1023 ? 1023 : count);
+                        }
+                        return collection;
+                    })(),
+                    mappingFn : function(element) {
+                        return Math.pow(2, element);
+                    }
+                }, callback);
+            }];
             //  ... more test cases, if desired
+
+            testCases.reduce(function(pV, cV) {
+                return pV.then(function() {
+                    return promiseTest(cV);
+                });
+            }, Promise.resolve()).then(function() {
+                console.log("All tests completed successfully.");
+            }).catch(function() {
+                console.error("Tests failed!");
+            });
         });
 });
--- test.js
+++ test.js
@@ -1,3 +1,11 @@
+function promiseTest(testInvocation) {
+    return new Promise(function testExecutor(resolve, reject) {
+        testInvocation(function(success) {
+            success ? resolve(success) : reject(success);
+        });
+    });
+}
+
 var runTest = (function() {
     'use strict';
 
@@ -6,7 +14,7 @@ var runTest = (function() {
     function clickHandler() {
         clicks++;
     }
-    function runTest(options) {
+    function runTest(options, callback) {
         options = options || {};
 
         clicks = 0;
@@ -17,9 +25,16 @@ var runTest = (function() {
         var intervalId;
 
         yieldingMap(options.collection, options.mappingFn, options.ymOptions).
-            then(function testVerify() {
+            then(function testVerify(result) {
                 endTime = Date.now();
                 clearInterval(intervalId);
+
+                var correctness = options.collection.map(options.mappingFn).map(function(elem, index) {
+                    return elem === result[index];
+                }).reduce(function(pV, cV) {
+                    return pV && cV;
+                }, true);
+                console.log(`Test result: ${correctness ? 'success' : 'failure'}.`);
 
                 var numIntervals = Math.floor((endTime - beginTime) / 100);
 
@@ -30,6 +45,8 @@ var runTest = (function() {
                 }
                 console.log(`Number of intervals: ${numIntervals}`);
                 console.log(`Number of registered clicks: ${clicks}`);
+
+                return callback(correctness);
             });
 
         intervalId = setInterval(function() {
