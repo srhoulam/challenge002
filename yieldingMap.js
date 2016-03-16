66fd6ff7e72ca21f39a1a732f55d1ec26e1bfe23
Saad Rhoulam <saad@rhoulam.com>
2016-03-16 16:33:23 -0400

    Have `yieldingMap` take performance options for bonus points.

--- yieldingMap.js
+++ yieldingMap.js
@@ -21,8 +21,16 @@ function yieldingMap(collection, mappingFn, options) {
             }
 
             //  effectively loop body and increment statement
-            result.push(mappingFn(collection[count++]));
-            setTimeout(step);
-        });
+            if(options.batchSize && options.batchSize > 1) {
+                for(var index = 0; index < options.batchSize && count < length; index++, count++) {
+                    result.push(mappingFn(collection[count]));
+                }
+            } else {
+                result.push(mappingFn(collection[count++]));
+            }
+
+
+            setTimeout(step, options.delayPeriod || 0);
+        }, options.delayPeriod || 0);
     });
 }
